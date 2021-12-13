72b5fc4992e65d3492270a1dcca19d3abf165087
elvedin123 <elvedinc97@gmail.com>
2021-12-12 20:26:51 -0500

    added wireframe and title

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 PROJECT OVERVIEW
 - **App Title**:
-
+LEAGUE-O-PEDIA
 - **App Description**: 
 - **API**: 
 ![api image](./assets/screenshot.png)
 - **API Snippet**: 
 ![snippets](./assets/ss2.png)
 - **Wireframes**:  
+![wireframelink](https://whimsical.com/EiR7gX8hZgw1B2xqJAsCnS)
+![wireframephoto](./assets/wireframe.png)
 - **MVP**: 
 - **Post-MVP**: 
 - **Goals**: 
--- scratch.js
+++ scratch.js
@@ -1,7 +1,7 @@
-async function fetchData(champion) {
-  const url = "http://ddragon.leagueoflegends.com/cdn/9.13.1/data/en_US/champion.json";
-  const response = await axios.get(url);
+async function fetchChampion() {
+  const url = `http://ddragon.leagueoflegends.com/cdn/11.24.1/data/en_US/champion.json`;
+  const res = await axios.get(url);
 
-  console.log(response.data);
+  console.log(res.data.data);
 }
-fetchData("aatrox")
+fetchChampion();
