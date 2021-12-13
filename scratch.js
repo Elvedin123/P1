235f7e25ff35761a7fc059a169213fc84a1558f5
elvedin123 <elvedinc97@gmail.com>
2021-12-13 15:41:50 -0500

    created async function to fetch champions

--- scratch.js
+++ scratch.js
@@ -1,7 +1,25 @@
-async function fetchChampion() {
+let inputBtn = document.querySelector("#search");
+let searchBar = document.querySelector("#input");
+let section = document.querySelector("#data-container");
+let champSelector = document.querySelector("#champion-select")
+let button = document.querySelector("#search");
+let champPhoto = document.querySelector("#champion-photo");
+
+async function fetchChampions() {
   const url = `http://ddragon.leagueoflegends.com/cdn/11.24.1/data/en_US/champion.json`;
   const res = await axios.get(url);
-
-  console.log(res.data.data);
+  console.log(res.data.data)
+  let champions = Object.keys(res.data.data)
+  console.log(champions)
 }
-fetchChampion();
+fetchChampions();
+
+function setChampionValues(champions) {
+  champions.forEach(champion => {
+    let option = document.createElement("option");
+    option.value = champion;
+    option.textContent = champion;
+
+
+  });
+}
