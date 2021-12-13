89599fd52a2dbcf4ee9c84ea502e69b97b9b9c59
elvedin123 <elvedinc97@gmail.com>
2021-12-13 18:22:41 -0500

    added photos for champion on submit bar selection

--- scratch.js
+++ scratch.js
@@ -4,14 +4,14 @@ let searchBar = document.querySelector("#input");
 let section = document.querySelector("#data-container");
 let champSelector = document.querySelector("#champion-select")
 let button = document.querySelector("#search");
-let champPhoto = document.querySelector("#champion-photo");
+let champPhoto = document.querySelector("#champion-image");
 // finds all champions
 async function fetchChampions() {
   const url = `http://ddragon.leagueoflegends.com/cdn/11.24.1/data/en_US/champion.json`;
   const res = await axios.get(url);
-  console.log(res.data.data)
+  // console.log(res)
   let champions = Object.keys(res.data.data)
-  console.log(champions)
+  // console.log(champions)
 
   setChampionValues(champions)
 }
@@ -19,7 +19,7 @@ fetchChampions();
 // sets champion values
 function setChampionValues(champions) {
   champions.forEach(champion => {
-    console.log(champion)
+    // console.log(champion)
     let option = document.createElement("option");
     option.value = champion;
     option.textContent = champion;
@@ -27,4 +27,15 @@ function setChampionValues(champions) {
 
   });
 
+};
+function fetchChampionImages(champion) {
+  // let champion = champSelector.value;
+  const imageUrl = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion}_0.jpg`;
+  // const res = await axios.get(url);
+  const image = document.createElement("img");
+  image.src = `${imageUrl}`;
+  image.alt = `${champion} splashart`;
+  champPhoto.appendChild(image);
+  // console.log(res)
 }
+champSelector.addEventListener("change", fetchChampionImages)
